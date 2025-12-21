94e0e5ba48a766749be9f2f3f064b2bfe682f11a
spinachmilk <bobkid5000@gmail.com>
2025-12-20 22:18:28 -0500

    stage 2

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]) {
 
   char command[1024];
   fgets(command, sizeof(command), stdin);
+  command[strcspn(command, "\n")] = 0;
   printf("%s: command not found", command);
 
   return 0;
