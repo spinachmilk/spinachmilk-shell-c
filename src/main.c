9a63d033043ed7cb14214282abddc0f2990433d9
spinachmilk <bobkid5000@gmail.com>
2025-12-20 22:19:48 -0500

    stage 2

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -8,8 +9,8 @@ int main(int argc, char *argv[]) {
 
   char command[1024];
   fgets(command, sizeof(command), stdin);
-  command[strcspn(command, "\n")] = 0;
-  printf("%s: command not found", command);
+  command[strcspn(command, "\n")] = '\0';
+  printf("%s: command not found\n", command);
 
   return 0;
 }
