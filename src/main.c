94238260075aa82d4bff66528443e6d021d19073
spinachmilk <bobkid5000@gmail.com>
2025-12-20 23:37:54 -0500

    stage 5

--- src/main.c
+++ src/main.c
@@ -6,12 +6,22 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   char command[1024];
+  char command_copy[1024];
   while (1){
     printf("$ ");
     fgets(command, sizeof(command), stdin);
     command[strcspn(command, "\n")] = '\0';
+    strcpy(command_copy, command);
+    // exit logic
     if (strcmp(command, "exit") == 0){
       break;
+    }
+    // echo logic
+    char *first_word = strtok(command_copy, " ");
+    char *rest_of_string = first_word + strlen(first_word) + 1;
+    if (strcmp(first_word, "echo") == 0){
+      printf("%s\n", rest_of_string);
+      continue;
     }
     printf("%s: command not found\n", command);
   }
