86e8ce7afb0454484466cbcbf69c405d08534884
spinachmilk <bobkid5000@gmail.com>
2025-12-20 22:57:21 -0500

    stage 4

--- src/main.c
+++ src/main.c
@@ -5,12 +5,14 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+  char command[1024];
   while (1){
     printf("$ ");
-
-    char command[1024];
     fgets(command, sizeof(command), stdin);
     command[strcspn(command, "\n")] = '\0';
+    if (strcmp(command, "exit") == 0){
+      break;
+    }
     printf("%s: command not found\n", command);
   }
   return 0;
