1dc1540fed6e95156a6e79e20198b2e0d1a242b1
spinachmilk <bobkid5000@gmail.com>
2025-12-23 03:31:28 -0500

    run external programs fr

--- src/main.c
+++ src/main.c
@@ -14,22 +14,30 @@ int main(int argc, char *argv[]) {
     fgets(command, sizeof(command), stdin);
     command[strcspn(command, "\n")] = '\0';
     strcpy(command_copy, command);
+    char *args[128];
+    int arg_count = 0;
+    char *token = strtok(command_copy, " ");
+    while (token != NULL && arg_count < 127){
+      args[arg_count++] = token;
+      token = strtok(NULL, " ");
+    }
+    args[arg_count] = NULL;
     // exit logic
-    if (strcmp(command, "exit") == 0){
+    if (strcmp(args[0], "exit") == 0){
       break;
     }
     // echo logic
-    char *first_word = strtok(command_copy, " ");
-    if (strcmp(first_word, "echo") == 0){
-      char *rest_of_string = command + 5;
-      printf("%s\n", rest_of_string);
+    if (strcmp(args[0], "echo") == 0){
+      for (int i = 1; i<arg_count; i++){
+        printf("%s", args[i]);
+      }
+      printf("\n");
       continue;
     }
     // type logic
-    if (strcmp(first_word, "type") == 0){
-      char *second_word = strtok(NULL, " ");
-      if (strcmp(second_word, "echo") == 0 || strcmp(second_word, "type") == 0 || strcmp(second_word, "exit") == 0){
-        printf("%s is a shell builtin\n", second_word);
+    if (strcmp(args[0], "type") == 0){
+      if (strcmp(args[1], "echo") == 0 || strcmp(args[1], "type") == 0 || strcmp(args[1], "exit") == 0){
+        printf("%s is a shell builtin\n", args[1]);
       } else{
         char *path_env = getenv("PATH");
         char *path_copy = strdup(path_env);
@@ -37,14 +45,13 @@ int main(int argc, char *argv[]) {
         char *dir = strtok(path_copy, ":");
         while (dir != NULL){
           char full_path[1024];
-          snprintf(full_path, sizeof(full_path), "%s/%s", dir, second_word);
+          snprintf(full_path, sizeof(full_path), "%s/%s", dir, args[1]);
           if (access(full_path, X_OK) == 0){
-            printf("%s is %s\n", second_word, full_path);
+            printf("%s is %s\n", args[1], full_path);
             found = 1;
             // Run external programs
             pid_t pid = fork();
             if (pid == 0){
-              char *args[] = {full_path, NULL};
               execv(full_path, args);
               exit(1);
             } else{
@@ -55,7 +62,7 @@ int main(int argc, char *argv[]) {
           dir = strtok(NULL, ":");
         }
         if (!found){
-          printf("%s: not found\n", second_word);
+          printf("%s: not found\n", args[1]);
         }
         free(path_copy);
       }
